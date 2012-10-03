f0face2c1ee899da082d16acc2d2838533fc6de0
Benjamin Gwynn <benjamin@gwynn.tk>
2012-10-03 11:54:48 +0200

    Why are we calling device/samsung/tuna?

--- device.mk
+++ device.mk
@@ -24,5 +24,7 @@ DEVICE_PACKAGE_OVERLAYS := device/samsung/maguro/overlay
 PRODUCT_COPY_FILES := \
 	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
+# TODO: Don't call device/samsung/tuna.
+
 $(call inherit-product, device/samsung/tuna/device.mk)
 $(call inherit-product-if-exists, vendor/samsung/maguro/device-vendor.mk)
